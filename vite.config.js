3ed6fbdbbd08e3649f2f6f3ff4e1fa7d8a45ab4b
Abishek Thakur <abishekthakur8862@gmail.com>
2025-03-31 12:11:21 +0530

    first mit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'https://ondc.alyajewels.com/'
+  // base:'https://ondc.alyajewels.com/'
+    // "proxy": "https://ondc.alyajewels.com/",
+  // "homepage": "https://ondc.alyajewels.com/",
 })
